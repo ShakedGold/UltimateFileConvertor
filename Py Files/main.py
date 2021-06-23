1a714ae4498f5697dc5b793625416ecf0ca8b248
Shaked Gold <shakedgoldy@gmail.com>
2021-06-23 17:14:37 +0300

    Added Video Conversion

--- Py Files/main.py
+++ Py Files/main.py
@@ -6,11 +6,12 @@ import future.moves.tkinter.font as font
 from future.moves import tkinter
 from future.moves.tkinter import filedialog
 from PIL import Image
+from moviepy.video.io.VideoFileClip import VideoFileClip
+from moviepy.video.VideoClip import ImageClip
+from moviepy.video.compositing.CompositeVideoClip import CompositeVideoClip
 
 root = tkinter.Tk()
-root.geometry("300x400")
 root.configure(bg='lightblue')
-root.title("Convert Images")
 
 windowWidth = root.winfo_reqwidth()
 windowHeight = root.winfo_reqheight()
@@ -21,31 +22,47 @@ root.geometry("+{}+{}".format(positionRight, positionDown))
 
 
 def __init__():
-    global items, fonts, textVars
+    global items, fonts, textVars, start
 
     # Dictionary
     textVars = {
+        "startLabelVar": tkinter.StringVar(),
         "importFileLabelVar": tkinter.StringVar()
     }
     textVars.get("importFileLabelVar").set("Files Selected:")
+    textVars.get("startLabelVar").set("Choose What Type Of Files \nYou Would Like To \nConvert To(Videos / Images):")
 
     fonts = {
         "importFileFont": font.Font(family='Assistant Bold', size=15),
         "cancelFont": font.Font(family='Assistant Bold', size=12),
-        "fileLabelFont": font.Font(family='Assistant Bold', size=12)
+        "fileLabelFont": font.Font(family='Assistant Bold', size=12),
+        "startLabelFont": font.Font(family='Assistant Bold', size=12),
+        "startImage": font.Font(family='Assistant Bold', size=12),
+        "startVideo": font.Font(family='Assistant Bold', size=12)
+    }
+    start = {
+        "image": tkinter.Button(root, text="     Convert Images     ", bg="#0373fc", activebackground='#0373fc',
+                                font=fonts.get("importFileFont"), borderwidth=0, command=imageStart),
+        "video": tkinter.Button(root, text="     Convert Videos     ", bg="#0373fc", activebackground='#0373fc',
+                                font=fonts.get("importFileFont"), borderwidth=0, command=videoStart)
     }
     items = {
         "labels": {
+            "startLabel": tkinter.Label(root, text=textVars.get("startLabelVar").get(),
+                                        relief=tkinter.RAISED,
+                                        borderwidth=0,
+                                        font=fonts.get("startLabelFont"),
+                                        bg='lightblue'),
             "importFileLabel": tkinter.Label(root, text=textVars.get("importFileLabelVar").get(),
                                              relief=tkinter.RAISED,
                                              borderwidth=0,
                                              font=fonts.get("fileLabelFont"),
                                              bg='lightblue')
         },
-        "buttons": {
+        "ImageButtons": {
             "importButton": tkinter.Button(root, text="   Import Files    ", bg="#0373fc", activebackground='#0373fc',
                                            font=fonts.get("importFileFont"), borderwidth=0,
-                                           command=lambda: clickEventImport("<Button-1>")),
+                                           command=lambda: clickEventImportImage("<Button-1>")),
             "convertButtonPNG": tkinter.Button(root, text="     Convert To PNG      ", bg="#0373fc",
                                                activebackground='#0373fc',
                                                font=fonts.get("importFileFont"), borderwidth=0,
@@ -64,66 +81,161 @@ def __init__():
                                                 command=lambda: convertImageTo("webp")),
             "cancel": tkinter.Button(root, text=" Cancel ", bg="#0373fc", activebackground='#0373fc',
                                      font=fonts.get("cancelFont"),
-                                     borderwidth=0, command=lambda: restart())
+                                     borderwidth=0, command=lambda: restartImage()),
+            "back": tkinter.Button(root, text=" Back ", bg="#0373fc", activebackground='#0373fc',
+                                   font=fonts.get("cancelFont"),
+                                   borderwidth=0, command=lambda: startScreen())
+        },
+        "VideoButtons": {
+            "importButton": tkinter.Button(root, text="   Import Files    ", bg="#0373fc", activebackground='#0373fc',
+                                           font=fonts.get("importFileFont"), borderwidth=0,
+                                           command=lambda: clickEventImportVideo("<Button-1>")),
+            "convertButtonMP4": tkinter.Button(root, text="   Convert To MP4    ", bg="#0373fc",
+                                               activebackground='#0373fc',
+                                               font=fonts.get("importFileFont"), borderwidth=0,
+                                               command=lambda: convertVideoTo("mp4")),
+            "convertButtonMOV": tkinter.Button(root, text="   Convert To MOV    ", bg="#0373fc",
+                                               activebackground='#0373fc',
+                                               font=fonts.get("importFileFont"), borderwidth=0,
+                                               command=lambda: convertVideoTo("mov")),
+            "convertButtonAVI": tkinter.Button(root, text="    Convert To AVI     ", bg="#0373fc",
+                                               activebackground='#0373fc',
+                                               font=fonts.get("importFileFont"), borderwidth=0,
+                                               command=lambda: convertVideoTo("avi")),
+            "convertButtonMKV": tkinter.Button(root, text="   Convert To MKV    ", bg="#0373fc",
+                                               activebackground='#0373fc',
+                                               font=fonts.get("importFileFont"), borderwidth=0,
+                                               command=lambda: convertVideoTo("mkv")),
+            "cancel": tkinter.Button(root, text=" Cancel ", bg="#0373fc", activebackground='#0373fc',
+                                     font=fonts.get("cancelFont"),
+                                     borderwidth=0, command=lambda: restartVideo()),
+            "back": tkinter.Button(root, text=" Back ", bg="#0373fc", activebackground='#0373fc',
+                                   font=fonts.get("cancelFont"),
+                                   borderwidth=0, command=lambda: startScreen())
         }
     }
+    startScreen()
 
-    items.get("buttons").get("importButton").pack(side=tkinter.TOP, expand=tkinter.YES)
+
+def imageStart():
+    root.geometry("300x400")
+    root.title("Select Image Files")
+    clearScreen()
+    items.get("ImageButtons").get("importButton").pack(side=tkinter.TOP, expand=tkinter.YES)
     items.get("labels").get("importFileLabel").pack(side=tkinter.TOP, expand=tkinter.YES)
-    for item in items.get("buttons"):
-        if item == "cancel": break
-        items.get("buttons").get(item).pack(side=tkinter.TOP, expand=tkinter.YES)
+    for item in items.get("ImageButtons"):
+        if item == "cancel":
+            continue
+        items.get("ImageButtons").get(item).pack(side=tkinter.TOP, expand=tkinter.YES)
+    items.get("labels").get("importFileLabel").config(text="Files Selected: ")
+    items.get("ImageButtons").get("importButton").config(state=tkinter.NORMAL, bg="#0373fc")
+
+
+def videoStart():
+    root.geometry("300x400")
+    root.title("Select Video Files")
+    clearScreen()
+    items.get("VideoButtons").get("importButton").pack(side=tkinter.TOP, expand=tkinter.YES)
+    items.get("labels").get("importFileLabel").pack(side=tkinter.TOP, expand=tkinter.YES)
+    for item in items.get("VideoButtons"):
+        if item == "cancel":
+            continue
+        items.get("VideoButtons").get(item).pack(side=tkinter.TOP, expand=tkinter.YES)
+    items.get("labels").get("importFileLabel").config(text="Files Selected: ")
+    items.get("VideoButtons").get("importButton").config(state=tkinter.NORMAL, bg="#0373fc")
+
+
+def startScreen():
+    clearScreen()
+    root.geometry("250x250")
+    root.title("Start Screen")
+    items.get("labels").get("startLabel").pack(side=tkinter.TOP, expand=tkinter.YES)
+    for startItem in start.keys():
+        start.get(startItem).pack(side=tkinter.TOP, expand=tkinter.YES)
 
 
 def clearScreen():
+    for item in start.keys():
+        start.get(item).pack_forget()
     for item in items.keys():
         for name in items.get(item):
             items.get(item).get(name).pack_forget()
 
 
-def restart():
+def restartVideo():
     clearScreen()
-    __init__()
+    videoStart()
+
+
+def restartImage():
+    clearScreen()
+    imageStart()
 
 
 def changeFileText(text):
     items.get("labels").get("importFileLabel").config(text=text)
-    items.get("buttons").get("importButton").config(state=tkinter.NORMAL, bg="#0373fc")
+    items.get("ImageButtons").get("importButton").config(state=tkinter.NORMAL, bg="#0373fc")
 
 
 def convertImageTo(filetype):
-    try:
-        exportLoc = filedialog.askdirectory()
-        for filename in files:
-            temp = filename
-            filename = filename.strip()
-            os.rename(temp, filename)
-            Image.open(filename).convert('RGB').save(
-                exportLoc + "/" + re.search("(.*)[.](.*)", filename.split("/")[-1]).group(1) + "." + filetype)
-            items.get("labels").get("importFileLabel").config(text="Completed Converting")
-            items.get("buttons").get("cancel").pack_forget()
+    exportLoc = filedialog.askdirectory()
+    if exportLoc == "":
+        clearScreen()
+        imageStart()
+        return
+    for filename in files:
+        temp = filename
+        filename = filename.strip()
+        os.rename(temp, filename)
+        Image.open(filename).convert('RGB').save(
+            exportLoc + "/" + re.search("(.*)[.](.*)", filename.split("/")[-1]).group(1) + "." + filetype)
+        items.get("labels").get("importFileLabel").config(text="Completed Converting")
+        items.get("ImageButtons").get("cancel").pack_forget()
 
-            # timer for 5 secs
-            suc = threading.Timer(4, lambda: changeFileText("Files Selected:"))
-            suc.start()
-    except:
-        items.get("labels").get("importFileLabel").config(text="An Error Occurred")
-        items.get("buttons").get("cancel").pack_forget()
-        err = threading.Timer(4, lambda: changeFileText("Files Selected:"))
-        err.start()
+        # timer for 5 secs
+        suc = threading.Timer(4, lambda: changeFileText("Files Selected:"))
+        suc.start()
 
 
-def newInit():
+def convertVideoTo(filetype):
+    items.get("labels").get("importFileLabel").config(text="Converting...\n(Its OK the program is not responding)")
+    exportLoc = filedialog.askdirectory()
+    print(exportLoc)
+    if exportLoc == "":
+        clearScreen()
+        videoStart()
+        return
+    for file in files:
+        clip = VideoFileClip(file)
+        clip.write_videofile(exportLoc + "/" + re.search("(.*)[.](.*)", file.split("/")[-1]).group(1) + "." + filetype, codec="libx264")
+    items.get("labels").get("importFileLabel").config(text="Done Converting!")
+    suc = threading.Timer(4, lambda: restartVideo())
+    suc.start()
+
+
+def selectedFilesImage():
     clearScreen()
-    items.get("buttons").get("importButton").config(bg="lightgray", state=tkinter.DISABLED)
-    items.get("buttons").get("importButton").pack(side=tkinter.TOP, expand=tkinter.YES)
-    items.get("buttons").get("cancel").pack(side=tkinter.TOP, expand=tkinter.YES)
+    root.geometry("300x450")
+    items.get("ImageButtons").get("importButton").config(state=tkinter.DISABLED, bg="gray")
+    items.get("ImageButtons").get("importButton").pack(side=tkinter.TOP, expand=tkinter.YES)
+    items.get("ImageButtons").get("cancel").pack(side=tkinter.TOP, expand=tkinter.YES)
     items.get("labels").get("importFileLabel").pack(side=tkinter.TOP, expand=tkinter.YES)
-    for item in items.get("buttons"):
-        items.get("buttons").get(item).pack(side=tkinter.TOP, expand=tkinter.YES)
+    for item in items.get("ImageButtons").keys():
+        items.get("ImageButtons").get(item).pack(side=tkinter.TOP, expand=tkinter.YES)
 
 
-def clickEventImport(event):
+def selectedFilesVideo():
+    clearScreen()
+    root.geometry("300x450")
+    items.get("VideoButtons").get("importButton").config(state=tkinter.DISABLED, bg="gray")
+    items.get("VideoButtons").get("importButton").pack(side=tkinter.TOP, expand=tkinter.YES)
+    items.get("VideoButtons").get("cancel").pack(side=tkinter.TOP, expand=tkinter.YES)
+    items.get("labels").get("importFileLabel").pack(side=tkinter.TOP, expand=tkinter.YES)
+    for item in items.get("VideoButtons").keys():
+        items.get("VideoButtons").get(item).pack(side=tkinter.TOP, expand=tkinter.YES)
+
+
+def clickEventImportImage(event):
     global files
     files = filedialog.askopenfilenames(initialdir="/", title="Select Files", filetypes=(("Image Files",
                                                                                           "*.png"),
@@ -135,14 +247,38 @@ def clickEventImport(event):
                                                                                           "*.webp"),
                                                                                          ("All Files",
                                                                                           "*.*")))
-    s = "File Selected: "
+    s = "Files Selected: "
     if len(files) <= 3:
         for file in files:
             s += "\n" + file.split("/")[-1]
-    else: s = "Multiple Files Selected"
+    else:
+        s = "Multiple Files Selected"
     textVars.get("importFileLabelVar").set(s)
     items.get("labels").get("importFileLabel").config(text=textVars.get("importFileLabelVar").get())
-    newInit()
+    selectedFilesImage()
+
+
+def clickEventImportVideo(event):
+    global files
+    files = filedialog.askopenfilenames(initialdir="/", title="Select Files", filetypes=(("Image Files",
+                                                                                          "*.mp4"),
+                                                                                         ("Image Files",
+                                                                                          "*.avi"),
+                                                                                         ("Image Files",
+                                                                                          "*.mov"),
+                                                                                         ("Image Files",
+                                                                                          "*.mkv"),
+                                                                                         ("All Files",
+                                                                                          "*.*")))
+    s = "Files Selected: "
+    if len(files) <= 3:
+        for file in files:
+            s += "\n" + file.split("/")[-1]
+    else:
+        s = "Multiple Files Selected"
+    textVars.get("importFileLabelVar").set(s)
+    items.get("labels").get("importFileLabel").config(text=textVars.get("importFileLabelVar").get())
+    selectedFilesVideo()
 
 
 __init__()
